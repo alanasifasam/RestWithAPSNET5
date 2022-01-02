0ff8ac7b96cb6bcd650a02018579bd80d32d7f6e
Alana <alanasifasam@gmail.com>
2022-01-01 21:54:54 -0300

    modificação de controller e criação de classe person

---
+++ RestWithAPSNET5/Controllers/PersonController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithAPSNET5.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+
+        private readonly ILogger<PersonController> _logger;
+
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult sum(string firstNumber, string secondNumber)
+        {
+           
+
+            return BadRequest("Invalid Input");
+        }
+
+
+    }
+}
