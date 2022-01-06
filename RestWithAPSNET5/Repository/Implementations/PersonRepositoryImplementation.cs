6fea45b1254e20be660d4340662d326d19198a02
Alana <alanasifasam@gmail.com>
2022-01-06 15:23:34 -0300

    Migrations

--- RestWithAPSNET5/Repository/Implementations/PersonRepositoryImplementation.cs
+++ RestWithAPSNET5/Repository/Implementations/PersonRepositoryImplementation.cs
@@ -47,7 +47,7 @@ namespace RestWithAPSNET5.Repository.Implementations
 
         public Person Update(Person person)
         {
-            if (!Exists(person.Id)) return new Person();
+            if (!Exists(person.Id)) return null;
 
             var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(person.Id));
             if (result != null)
--- RestWithAPSNET5/Startup.cs
+++ RestWithAPSNET5/Startup.cs
@@ -12,19 +12,27 @@ using RestWithAPSNET5.Business.Implementations;
 using RestWithAPSNET5.Model.Context;
 using RestWithAPSNET5.Repository;
 using RestWithAPSNET5.Repository.Implementations;
+using Serilog;
+using System;
+using System.Collections.Generic;
 
 
 
 namespace RestWithAPSNET5
 {
+
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
         {
             Configuration = configuration;
+            Environment = environment;
+
+            Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
         }
 
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -33,11 +41,15 @@ namespace RestWithAPSNET5
             services.AddControllers();
 
             var connection = Configuration["MySQLConnetion:MySQLConnetionString"];
-            
-            services.AddDbContext<MySQLContext>(options => options.UseMySql(connection,ServerVersion.AutoDetect(connection)));
+            services.AddDbContext<MySQLContext>(options => options.UseMySql(connection, ServerVersion.AutoDetect(connection)));
+
+            if (Environment.IsDevelopment())
+            {
+                MigrateDatabase(connection);
+            }
 
 
-            services.AddApiVersioning();    
+            services.AddApiVersioning();
 
 
             //Dependecy Injection 
@@ -49,6 +61,10 @@ namespace RestWithAPSNET5
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "RestWithAPSNET5", Version = "v1" });
             });
         }
+
+
+
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -70,6 +86,25 @@ namespace RestWithAPSNET5
             {
                 endpoints.MapControllers();
             });
+        }
+
+        private void MigrateDatabase(string connection)
+        {
+            try
+            {
+                var evolveConnection = new MySql.Data.MySqlClient.MySqlConnection(connection);
+                var evolve = new Evolve.Evolve(evolveConnection, msg => Log.Information(msg))
+                {
+                    Locations = new List<string> { "db/migrations", "db/dataset" },
+                    IsEraseDisabled = true,
+                };
+                evolve.Migrate();
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Database migration failed", ex);
+                throw;
+            }
         }
     }
 }
